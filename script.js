298595f3a12a52299a29435ca59e51f7d7fbdab1
Motiullah Sajit <sajitmotiullah@gamil.com>
2021-05-10 08:43:34 +0600

    accessories section added

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const bannerImgs = ['images/banner2.webp', 'images/banner2.webp', 'images/banner4.webp'];
 
 const productsData = [{
-    title: 'Demo Product title',
+    name: 'Demo Product name',
     category: 'Demo category',
     price: 80.00,
     image: 'images/product1.png'
 },
 {
-    title: 'Demo Product title',
+    name: 'Demo Product name',
     category: 'Demo category',
     price: 29.00,
     image: 'images/product2.jpg'
 },
 {
-    title: 'Demo Product title',
+    name: 'Demo Product name',
     category: 'Demo category',
     price: 50.00,
     image: 'images/product3.png'
 },
 {
-    title: 'Demo Product title',
+    name: 'Demo Product name',
     category: 'Demo category',
     price: 80.00,
     image: 'images/product4.jpg'
 },
 {
-    title: 'Demo Product title',
+    name: 'Demo Product name',
     category: 'Demo category',
     price: 519.00,
     image: 'images/product5.jpg'
@@ -55,8 +55,45 @@ const categoriesData = [{
     name: 'Mallet',
     image: 'images/category6.jpg'
 },
-]
+];
 
+const accessoriesData = [{
+    category: 'Demo category',
+    name: 'Dummy Product Name',
+    image: 'images/accessories2.jpg',
+    price: 39.00
+},
+{
+    category: 'Demo category',
+    name: 'Dummy Product Name',
+    image: 'images/accessories3.jpg',
+    price: 50.00
+},
+{
+    category: 'Demo category',
+    name: 'Dummy Product Name',
+    image: 'images/accessories4.jpg',
+    price: 29.00
+},
+{
+    category: 'Demo category',
+    name: 'Dummy Product Name',
+    image: 'images/accessories5.jpg',
+    price: 55.00
+},
+{
+    category: 'Demo category',
+    name: 'Dummy Product Name',
+    image: 'images/accessories7.jpg',
+    price: 79.00
+},
+{
+    category: 'Demo category',
+    name: 'Dummy Product Name',
+    image: 'images/accessories7.jpg',
+    price: 29.00
+}
+]
 
 const banner = document.getElementById('banner');
 bannerImgs.map(bannerimg => {
@@ -70,15 +107,15 @@ bannerImgs.map(bannerimg => {
 
 const hotDeals = document.getElementById('hot-deals');
 productsData.map(product => {
-    const { title, category, price, image } = product;
+    const { name, category, price, image } = product;
     const productCard = document.createElement('div');
     productCard.className = 'col-lg-2 col-md-4 col-sm-12';
-    productCard.innerHTML = `<div class="card">
-    <img src=${image} class="card-img-top w-100" alt=${title}>
+    productCard.innerHTML = `<div class="card shadow">
+    <img src=${image} class="card-img-top w-100" alt=${name}>
     <div class="card-body">
-      <h6 class="card-title">${category}</h6>
-      <h6 class="card-title">${title}</h6>
-      <h5 class="card-title text-warning"> $ ${price}</h5>
+      <hp class="card-title">${category}</p>
+      <h6 class="card-title">${name}</h6>
+      <h5 class="card-title text-brand"> $ ${price}</h5>
     </div>
   </div>`;
     hotDeals.appendChild(productCard);
@@ -86,15 +123,15 @@ productsData.map(product => {
 
 const newArrivals = document.getElementById('new-arrivals');
 productsData.map(product => {
-    const { title, category, price, image } = product;
+    const { name, category, price, image } = product;
     const productCard = document.createElement('div');
     productCard.className = 'col-lg-2 col-md-4 col-sm-12';
-    productCard.innerHTML = `<div class="card">
-    <img src=${image} class="card-img-top w-100" alt=${title}>
+    productCard.innerHTML = `<div class="card shadow">
+    <img src=${image} class="card-img-top w-100" alt=${name}>
     <div class="card-body">
-      <h6 class="card-title">${category}</h6>
-      <h6 class="card-title">${title}</h6>
-      <h5 class="card-title text-warning"> $ ${price}</h5>
+      <p class="card-title">${category}</p>
+      <h6 class="card-title">${name}</h6>
+      <h5 class="card-title text-brand"> $ ${price}</h5>
     </div>
   </div>`;
     newArrivals.appendChild(productCard);
@@ -113,3 +150,37 @@ categoriesData.map(category => {
     categories.appendChild(categoryCard);
 });
 
+const featuredAccessory = document.getElementById('featured-accessory')
+featuredAccessory.innerHTML = ` <div class="card">
+<img src="images/accessories1.jpg" class="card-img-top" alt="accessory img">
+<div class="card-body">
+    <h6 class="card-name">Accessories</h6>
+    <h5 class="card-name">Dummy Product Name</h5>
+    <div class="d-flex justify-content-between">
+        <div class="ratings d-flex">
+            <i class="far fa-star"></i>
+            <i class="far fa-star"></i>
+            <i class="far fa-star"></i>
+            <i class="far fa-star"></i>
+            <i class="far fa-star"></i>
+        </div>
+        <h3 class="text-brand">$ 79</h3>
+    </div>
+</div>
+</div>`;
+
+const accessories = document.getElementById('accessories');
+accessoriesData.map(accessory => {
+    const { name, category, price, image } = accessory;
+    const accessoryCard = document.createElement('div');
+    accessoryCard.className = 'col-lg-4 col-md-6 col-sm-12';
+    accessoryCard.innerHTML = `<div class="card mb-2">
+    <img src=${image} class="card-img-top w-100" alt=${name}>
+    <div class="card-body">
+      <h6 class="card-name">${category}</h6>
+      <h6 class="card-name">${name}</h6>
+      <h5 class="card-name text-brand"> $ ${price}</h5>
+    </div>
+  </div>`;
+    accessories.appendChild(accessoryCard);
+});
